8a119146beb54056df9ca1f2c033c58e6d20cfa2
jananisekar06 <jananisekar1729@gmail.com>
2026-07-12 14:13:56 +0530

    Update upload.js

--- js/upload.js
+++ js/upload.js
@@ -1,1 +1,15 @@
+function uploadFile() {
 
+    const file =
+    document.getElementById("fileInput").files[0];
+
+    if (!file) {
+
+        alert("Please Select a File");
+
+        return;
+    }
+
+    alert(file.name + " Uploaded Successfully");
+
+}
