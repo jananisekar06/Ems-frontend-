d52591b23ed2f4eccb844b5d3836454bf79781bf
jananisekar06 <jananisekar1729@gmail.com>
2026-07-12 14:13:10 +0530

    Update report.js

--- js/report.js
+++ js/report.js
@@ -1,1 +1,17 @@
+const employeeCount =
+JSON.parse(localStorage.getItem("employees")) || [];
 
+const attendanceCount =
+JSON.parse(localStorage.getItem("attendance")) || [];
+
+const leaveCount =
+JSON.parse(localStorage.getItem("leave")) || [];
+
+document.getElementById("empCount").textContent =
+employeeCount.length;
+
+document.getElementById("attCount").textContent =
+attendanceCount.length;
+
+document.getElementById("leaveCount").textContent =
+leaveCount.length;
