9af24e08db3cee97b08582f4336f244489428724
jananisekar06 <jananisekar1729@gmail.com>
2026-07-12 14:11:38 +0530

    Update notification.js

--- js/notification.js
+++ js/notification.js
@@ -1,1 +1,20 @@
+let notifications = [
+    "Attendance Updated Successfully",
+    "Leave Request Approved",
+    "Salary Credited",
+    "Meeting Scheduled Tomorrow"
+];
 
+const notificationList = document.getElementById("notificationList");
+
+notifications.forEach(message => {
+
+    const li = document.createElement("li");
+
+    li.className = "bg-indigo-100 p-4 rounded mb-3";
+
+    li.textContent = message;
+
+    notificationList.appendChild(li);
+
+});
