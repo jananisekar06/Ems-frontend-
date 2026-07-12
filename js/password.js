37ab648fa59e4212bcf653d07e7b9339d9bd8c2c
jananisekar06 <jananisekar1729@gmail.com>
2026-07-12 14:10:32 +0530

    Update password.js

--- js/password.js
+++ js/password.js
@@ -1,1 +1,19 @@
+function generatePassword() {
 
+    const chars =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$%&*";
+
+    let password = "";
+
+    for (let i = 0; i < 10; i++) {
+
+        password += chars.charAt(
+            Math.floor(Math.random() * chars.length)
+        );
+
+    }
+
+    document.getElementById("password").value =
+    password;
+
+}
